cf4aedfbbf64e2c56919678aafcae0934e1a1a04
Roman Skok <rma.skok@gmail.com>
2023-05-28 01:58:07 +0300

    feat: is_finished added to json

--- backend/tasks.go
+++ backend/tasks.go
@@ -19,6 +19,7 @@ type Task struct {
 	Title        string    `json:"title" gorm:"column:title" binding:"required"`
 	Shared       bool      `json:"shared" gorm:"column:shared"`
 	EstimateTime int       `json:"estimate_time,omitempty" gorm:"column:estimate_time"`
+	IsFinished   bool      `json:"is_finished" gorm:"column:is_finished"`
 	Description  string    `json:"description" gorm:"column:description"`
 	CreatedAt    time.Time `json:"createdAt,omitempty" gorm:"column:created_at"`
 }
