da626ce47d15a7d7e3c382aaa79234dc777324c9
Roman Skok <rma.skok@gmail.com>
2023-05-28 01:14:00 +0300

    feat: task estimate_time

--- backend/tasks.go
+++ backend/tasks.go
@@ -12,14 +12,15 @@ func (Task) TableName() string {
 }
 
 type Task struct {
-	ID          int       `json:"id,omitempty" gorm:"column:id"`
-	UserId      int       `json:"-" gorm:"column:user_id"`
-	Assignee    *int      `json:"-" gorm:"column:assignee"`
-	ProjectId   int       `json:"projectId" gorm:"column:project_id"`
-	Title       string    `json:"title" gorm:"column:title" binding:"required"`
-	Shared      bool      `json:"shared" gorm:"column:shared"`
-	Description string    `json:"description" gorm:"column:description"`
-	CreatedAt   time.Time `json:"createdAt,omitempty" gorm:"column:created_at"`
+	ID           int       `json:"id,omitempty" gorm:"column:id"`
+	UserId       int       `json:"-" gorm:"column:user_id"`
+	Assignee     *int      `json:"-" gorm:"column:assignee"`
+	ProjectId    int       `json:"projectId" gorm:"column:project_id"`
+	Title        string    `json:"title" gorm:"column:title" binding:"required"`
+	Shared       bool      `json:"shared" gorm:"column:shared"`
+	EstimateTime int       `json:"estimateTime,omitempty" gorm:"column:estimate_time"`
+	Description  string    `json:"description" gorm:"column:description"`
+	CreatedAt    time.Time `json:"createdAt,omitempty" gorm:"column:created_at"`
 }
 
 type TasksDB struct {
