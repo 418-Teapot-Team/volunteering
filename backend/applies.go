7b321870e5f43fc1ab5617a09a107a63fb2dc6a9
BoryslavGlov <boryslav.ziubrytskyi@providers.glovoapp.com>
2023-05-28 07:01:40 +0300

    fix: fix

--- backend/applies.go
+++ backend/applies.go
@@ -8,23 +8,12 @@ func (AppliesGetter) TableName() string {
 	return appliesTable
 }
 
-func (AppliedUserGetter) TableName() string {
-	return "users"
-}
-
 type Applies struct {
 	ID            int       `json:"-" gorm:"column:id"`
 	TaskID        int       `json:"taskId" gorm:"column:task_id"`
 	RespondUserID int       `json:"respondUserID" gorm:"column:respond_user_id"`
 	AppliedUserID int       `json:"appliedUserID,omitempty" gorm:"column:applied_user_id"`
 	CreatedAt     time.Time `json:"createdAt" gorm:"column:created_at"`
-}
-
-type AppliedUserGetter struct {
-	Id        int    `json:"id" gorm:"column:id"`
-	FirstName string `json:"firstName" binding:"required" gorm:"column:first_name"`
-	LastName  string `json:"lastName" binding:"required" gorm:"column:last_name"`
-	Email     string `json:"email" binding:"required" gorm:"column:email"`
 }
 
 type AppliesGetter struct {
